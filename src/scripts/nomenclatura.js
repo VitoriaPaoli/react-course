5f84702402eb3af68d5eb3921bef847bfa1d50e4
andrelucassilva <andrresilca@outlook.com>
2024-05-15 22:16:46 -0300

    feat: criado componente

---
+++ src/scripts/nomenclatura.js
@@ -0,0 +1,19 @@
+
+// nomenclatua de variavél
+
+const userName = "Vitoria Paoli"
+
+const user_name = "Vitoria Paoli"
+
+// nomenclatua de funções
+
+function SomaNumeros(a, b) {
+    return a + b
+}
+
+const primeiroNumero = 10
+
+const segundoNumero = 20
+
+console.log(SomaNumeros(primeiroNumero, segundoNumero))
+console.log(10 + 40)
